6d2ff91501bf53326f4a6428f9a1478c20f1f166
xxseva44 <dev.xxseva44@gmail.com>
2022-05-09 16:29:27 -0400

    Restore media side of commit: e83ad9f9e8a7a362e00779b7ce81ff3971e445d6, audio: support set volume for app

--- media/libaudioclient/include/media/AudioSystem.h
+++ media/libaudioclient/include/media/AudioSystem.h
@@ -53,7 +53,7 @@ typedef void (*audio_session_callback)(int event,
 class IAudioFlinger;
 class IAudioPolicyService;
 class String8;
-
+class AppTrackData;
 class AudioSystem
 {
 public:
@@ -518,6 +518,12 @@ public:
                                               audio_port_handle_t portId);
 
     static audio_port_handle_t getDeviceIdForIo(audio_io_handle_t audioIo);
+    
+    static status_t setAppVolume(const String8& packageName, const float value);
+    static status_t setAppMute(const String8& packageName, const bool value);
+    static status_t listAppTrackDatas(unsigned int *num, AppTrackData *vols);
+
+
 
 private:
 
